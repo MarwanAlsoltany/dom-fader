014aaf8032dfee6f5de419b5d9cb823d09a36d6a
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2020-09-09 06:13:34 +0200

    Fix links linking back to origin

--- src/fader.js
+++ src/fader.js
@@ -1,44 +1,49 @@
-(function (cssBase64DataURI) {
+(function(cssBase64DataURI) {
   "use strict";
-
-  var style = document.createElement("LINK");
-  style.rel = "stylesheet";
-  style.href = cssBase64DataURI || "data:text/css;base64,I2ZhZGVye3dpZHRoOjEwMCU7aGVpZ2h0OjEwMCU7cG9zaXRpb246Zml4ZWQ7dG9wOjA7cmlnaHQ6MDtib3R0b206MDtsZWZ0OjA7ei1pbmRleDo5OTk5OTk5O3BvaW50ZXItZXZlbnRzOm5vbmU7YmFja2dyb3VuZDojZmZmOy13ZWJraXQtYW5pbWF0aW9uLWR1cmF0aW9uOi4zczthbmltYXRpb24tZHVyYXRpb246LjNzOy13ZWJraXQtYW5pbWF0aW9uLXRpbWluZy1mdW5jdGlvbjplYXNlLWluLW91dDthbmltYXRpb24tdGltaW5nLWZ1bmN0aW9uOmVhc2UtaW4tb3V0fSNmYWRlci5mYWRlLW91dHtvcGFjaXR5OjA7LXdlYmtpdC1hbmltYXRpb24tbmFtZTpmYWRlLW91dDthbmltYXRpb24tbmFtZTpmYWRlLW91dH0jZmFkZXIuZmFkZS1pbntvcGFjaXR5OjE7LXdlYmtpdC1hbmltYXRpb24tbmFtZTpmYWRlLWluO2FuaW1hdGlvbi1uYW1lOmZhZGUtaW59QC13ZWJraXQta2V5ZnJhbWVzIGZhZGUtb3V0e2Zyb217b3BhY2l0eToxfXRve29wYWNpdHk6MH19QGtleWZyYW1lcyBmYWRlLW91dHtmcm9te29wYWNpdHk6MX10b3tvcGFjaXR5OjB9fUAtd2Via2l0LWtleWZyYW1lcyBmYWRlLWlue2Zyb217b3BhY2l0eTowfXRve29wYWNpdHk6MX19QGtleWZyYW1lcyBmYWRlLWlue2Zyb217b3BhY2l0eTowfXRve29wYWNpdHk6MX19";
-  document.head.appendChild(style);
-
+  // creating and injected fading effect element
   var fader = document.createElement("SVG");
   fader.id = "fader";
+  fader.className = "fade-out";
   document.body.insertBefore(fader, document.body.firstChild);
-
-  var script = document.getElementById("fader-script");
-  document.body.removeChild(script);
-
+  // injecting fader stylesheet
+  var style = document.createElement("LINK");
+  style.rel = "stylesheet";
+  style.href = cssBase64DataURI || "data:text/css;base64,OnJvb3R7LS1mYWRlci1jb2xvcjojZmZmfSNmYWRlcnt3aWR0aDoxMDAlO2hlaWdodDoxMDAlO3Bvc2l0aW9uOmZpeGVkO3RvcDowO3JpZ2h0OjA7Ym90dG9tOjA7bGVmdDowO3otaW5kZXg6OTk5OTk5OTtwb2ludGVyLWV2ZW50czpub25lO2JhY2tncm91bmQ6I2ZmZjtiYWNrZ3JvdW5kOnZhcigtLWZhZGVyLWNvbG9yKTstd2Via2l0LWFuaW1hdGlvbi1kdXJhdGlvbjouM3M7YW5pbWF0aW9uLWR1cmF0aW9uOi4zczstd2Via2l0LWFuaW1hdGlvbi10aW1pbmctZnVuY3Rpb246ZWFzZS1pbi1vdXQ7YW5pbWF0aW9uLXRpbWluZy1mdW5jdGlvbjplYXNlLWluLW91dH0jZmFkZXIuZmFkZS1vdXR7b3BhY2l0eTowOy13ZWJraXQtYW5pbWF0aW9uLW5hbWU6ZmFkZXItLWZhZGUtb3V0O2FuaW1hdGlvbi1uYW1lOmZhZGVyLS1mYWRlLW91dH0jZmFkZXIuZmFkZS1pbntvcGFjaXR5OjE7LXdlYmtpdC1hbmltYXRpb24tbmFtZTpmYWRlci0tZmFkZS1pbjthbmltYXRpb24tbmFtZTpmYWRlci0tZmFkZS1pbn1ALXdlYmtpdC1rZXlmcmFtZXMgZmFkZXItLWZhZGUtb3V0e2Zyb217b3BhY2l0eToxfXRve29wYWNpdHk6MH19QGtleWZyYW1lcyBmYWRlci0tZmFkZS1vdXR7ZnJvbXtvcGFjaXR5OjF9dG97b3BhY2l0eTowfX1ALXdlYmtpdC1rZXlmcmFtZXMgZmFkZXItLWZhZGUtaW57ZnJvbXtvcGFjaXR5OjB9dG97b3BhY2l0eToxfX1Aa2V5ZnJhbWVzIGZhZGVyLS1mYWRlLWlue2Zyb217b3BhY2l0eTowfXRve29wYWNpdHk6MX19";
+  document.head.appendChild(style);
+  // stop if AnimationEvent is not supported
   if (!window.AnimationEvent) {
     return;
   }
-
-  fader.classList.add("fade-out");
-
-  document.addEventListener("DOMContentLoaded", function () {
+  // attaching fade-out effect events to all anchors of the page
+  document.addEventListener("DOMContentLoaded", function() {
     var anchors = document.getElementsByTagName("a");
-
+    // extracting some anchors from the effect
     for (var i = 0; i < anchors.length; i++) {
       if (
-        anchors[i].hostname !== window.location.hostname ||
-        anchors[i].pathname === window.location.pathname ||
-        anchors[i].pathname === document.baseURI.replace(window.location.origin, '')
+        (
+          // anchors on the same pathname
+          anchors[i].hash.length &&
+          anchors[i].pathname === window.location.pathname
+        ) ||
+        (
+          // anchors linking back to origin
+          anchors[i].hash.length &&
+          anchors[i].pathname === document.baseURI.replace(window.location.origin, '')
+        ) ||
+        // anchors linking to a different origin
+        anchors[i].hostname !== window.location.hostname
       ) {
         continue;
       }
-
-      anchors[i].addEventListener("click", function (event) {
+      // attaching events to the targeted anchors
+      anchors[i].addEventListener("click", function(event) {
         var anchor = event.currentTarget;
-
+        // defer location change until the animation has finished
         var listener = function listener() {
           window.location = anchor.href;
           fader.removeEventListener("animationend", listener);
         };
-
+        // attaching the event to the anchor
         fader.addEventListener("animationend", listener);
         event.preventDefault();
         fader.classList.add("fade-in");
@@ -48,14 +53,16 @@
       });
     }
   });
-
-  window.addEventListener("pageshow", function (event) {
+  // stop fade effect for cached documents
+  window.addEventListener("pageshow", function(event) {
     if (!event.persisted) {
       return;
     }
-
-    var fader = document.getElementById("fader");
     fader.classList.remove("fade-in");
   });
-
+  // remove fader script from the document
+  var script = document.getElementById("fader-script");
+  if (script) {
+    document.body.removeChild(script);
+  }
 })();
